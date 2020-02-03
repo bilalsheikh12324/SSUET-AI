7797a0c8bb597e59765001fcd498ada14fc9b104
unknown <bilal.sheikh50.bs@gmail.com>
2020-02-03 20:24:19 +0500

    First Program

---
+++ Untitled.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Information")
+
+
+# In[10]:
+
+
+name="Bilal Sheikh"
+fathername="Sheikh Hamid"
+email="bilal.sheikh50.bs@gmail.com"
+age=22
+city="karachi"
+qualification="B.Sc"
+print("Name: ",name)
+print ("Father Name: ",fathername)
+print("Email: ",email)
+print("Age: ",age)
+print("City: ",city)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
